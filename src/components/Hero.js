9ac6faea73c2c334adc5f3f01cf6e9b7dc2f9a8e
kika <anamonteiro430@gmail.com>
2020-06-05 14:03:45 +0100

    update resume

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import linkedin from './../images/linkedin.svg';
-import github from './../images/github.svg';
+import React from "react";
+import linkedin from "./../images/linkedin.svg";
+import github from "./../images/github.svg";
 
 export const Hero = () => {
-	return (
-		<div className='hero'>
-			<div className='hero-title'>
-				<h1>
-					Hello, I'm <span className='square'>Ana</span>
-				</h1>
-				<h2>Fullstack Web Developer</h2>
-			</div>
-			<p className='bio'>
-				Nice human with a passion for creating applications and solving problems. <br />Take a look at my projects and feel free to contact me!
-			</p>
-			<div id='contact-icons'>
-				<a href="https://www.linkedin.com/in/anamonteiro430/" target="_blank"><img src={linkedin} className='linkedin' width='42' /></a>
-				<a href="https://github.com/anamonteiro430" target="_blank"><img src={github}
-					className='github' width='42' /></a>
+  return (
+    <div className="hero">
+      <div className="hero-title">
+        <h1>
+          Hello, I'm <span className="square">Ana</span>
+        </h1>
+        <h2>Fullstack Web Developer</h2>
+      </div>
+      <p className="bio">
+        Nice human with a passion for creating applications and solving
+        problems. <br />
+        Take a look at my projects and feel free to contact me!
+      </p>
+      <div id="contact-icons">
+        <a href="https://www.linkedin.com/in/anamonteiro430/" target="_blank">
+          <img src={linkedin} className="linkedin" width="42" />
+        </a>
+        <a href="https://github.com/anamonteiro430" target="_blank">
+          <img src={github} className="github" width="42" />
+        </a>
+      </div>
 
-			</div>
-
-			<a href='https://drive.google.com/file/d/1dJlgjcfyJGPwWKhNSpTY_-DVkESLqtrF/view' className='resume' target="_blank">
-				View my Resume
-			</a>
-		</div>
-	);
+      <a
+        href="https://drive.google.com/file/d/1Y1EJdTjSHDp34MiKQcdr-JWgjB1gRE_E/view"
+        className="resume"
+        target="_blank"
+      >
+        View my Resume
+      </a>
+    </div>
+  );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export const Nav = () => {
-	return (
-		<nav>
-			<h3 id="logo" ><a href="#App">Ana Francisca Monteiro</a></h3>
-			<div className='menu'>
-				<a href="https://drive.google.com/file/d/1dJlgjcfyJGPwWKhNSpTY_-DVkESLqtrF/view" target='_blank'>Resume</a>
-				<a href="#projects">Projects</a>
-				<a href="#contact">Contact</a>
-			</div>
-		</nav>
-	);
+  return (
+    <nav>
+      <h3 id="logo">
+        <a href="#App">Ana Francisca Monteiro</a>
+      </h3>
+      <div className="menu">
+        <a
+          href="https://drive.google.com/file/d/1Y1EJdTjSHDp34MiKQcdr-JWgjB1gRE_E/view"
+          target="_blank"
+        >
+          Resume
+        </a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
 };
